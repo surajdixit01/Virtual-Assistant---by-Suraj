8a4eecef926f538038bb2c6f4312b45daaba6db6
surajdixit01 <surajdixitvk18@gmail.com>
2025-09-07 22:17:02 +0530

    Update Customize.jsx

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -8,7 +8,7 @@ import image5 from "../assets/image5.png"
 import image6 from "../assets/image6.png"
 import image7 from "../assets/image3.png"
 import { RiImageAddLine } from "react-icons/ri";
-import { userDataContext } from '../context/userContext'
+import { userDataContext } from '../context/UserContext'
 import { Navigate, useNavigate } from 'react-router-dom'
 import { MdArrowBack } from "react-icons/md";
 
